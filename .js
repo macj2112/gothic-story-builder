ede8350db39b9aac791940fc33baea9929837f99
macj2112 <52211400+macj2112@users.noreply.github.com>
2024-09-19 10:16:19 -0400

    Create .js

---
+++ .js
@@ -0,0 +1,2 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Winwheel.js/2.7.0/Winwheel.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
